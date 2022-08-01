c705ce6b6c154b2512b51c86747c710d7d3556fd
Brayan Pichardo <brayanpichardo8@gmail.com>
2022-07-31 23:40:23 -0400

    added a simple registration page

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
+from crypt import methods
+from click import confirm
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from forms import RegistrationForm
+import os
 
+# create flask instance
 app = Flask(__name__)
+
+# create socketio instance 
 socketio = SocketIO(app)
+
+app.config['SECRET_KEY'] = 'your-key'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+
+    username = None
+    email = None
+    password = None 
+    confirm_password = None
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        confirm_password = form.confirm_password.data
+
+
+
+    return render_template('register.html', form=form, username=username, email=email, password=password, 
+                                            confirm_password=confirm_password)
+    
+
 
 # 3. listens for message sent by client
 @socketio.on('message')
@@ -18,3 +50,4 @@ def message_handler(data):
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
+
